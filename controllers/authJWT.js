7aff7ae1ea103c3e715a199086219901bb6717c9
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-04-25 02:10:26 +0700

    update

--- controllers/authJWT.js
+++ controllers/authJWT.js
@@ -1,5 +1,21 @@
 const mysql = require('mysql2/promise');
 
-const login = () =>{
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'student',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    password: process.env.SQL_PASSWORD
+});
 
+const login = async () => {
+    const promise = pool.promise();
+    const [rows,fields] = await promisePool.query("SELECT 1");
+    
+}
+
+module.exports = {
+    login
 }
